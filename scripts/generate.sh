1118b2428bb9f39f61a69d85afd6b30c144e4e80
ParitoshBh <mail@paritoshbh.me>
2023-02-14 22:27:31 -0500

    Prepare for automatic docker-compose.yml build

---
+++ scripts/generate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo 'Removing existing docker-compose.yml file'
+rm docker-compose.yml
+
+echo 'Creating temporary file to work on'
+cp templates/docker-compose.template docker-compose.yml
+
+echo 'Replacing app version'
+sed -i 's/APP_VERSION/0.1.6/g' docker-compose.yml
+
+echo 'Replacing jobber version'
+sed -i 's/JOBBER_VERSION/0.0.8/g' docker-compose.yml
+
+echo 'Finished processing'
