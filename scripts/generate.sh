367e9230f786a75563bd8233ba0ab2d27ae65e70
ParitoshBh <mail@paritoshbh.me>
2023-02-14 22:38:08 -0500

    Setup githib user for auto commit

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -12,7 +12,11 @@ sed -i 's/APP_VERSION/0.1.5/g' docker-compose.yml
 echo 'Replacing jobber version'
 sed -i 's/JOBBER_VERSION/0.0.7/g' docker-compose.yml
 
+echo 'Setting up github user for auto-commit'
+git config user.name github-actions
+git config user.email github-actions@github.com
+
 echo 'Commit docker-compose.yml'
 git add docker-compose.yml
-git commit -m "Bump version" docker-compose.yml
+git commit -m "chore: Bump version" docker-compose.yml
 git push
